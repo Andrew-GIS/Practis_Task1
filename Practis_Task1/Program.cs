5f2f16359c19b40bf2ae335f3ba25005032bb3f3
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-03-30 15:01:39 +0200

    1.5

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -10,36 +10,22 @@ namespace Practis_Task1
     {
         static void Main(string[] args)
         {
-            //task 1
-            int uberful = 5;
-            int _Identifier = 4;
-            int \u006fldenifier;
-            int myVariab1le;
+            int i1 = 455;
+            int i2 = 84500;
+            decimal dec = 7.98845m;
 
-            //task 2
-            double pi = 3.141592653;
-            double e = 2.7182818284590452;
-            Console.WriteLine($"Pi = {pi}, E = {e}");
-            //task 3
-
-            string first = "\nмоя строка 1";
-            string second = "\tмоя строка 2";
-            string third = "\aмоя строка 3";
-            Console.WriteLine(first);
-            Console.WriteLine(second);
-            Console.WriteLine(third);
-
+            //
+            //приводим два числа типа int к типу short
+            //
+            Console.WriteLine((short)i1);
+            Console.WriteLine((short)i2);
+            //
+            //приводим два числа типа decimal к типу int
+            //
+            Console.WriteLine((int)dec);
+            Console.ReadLine ();
         }
 
-        public static decimal PerformCalcul(int r)
-
-        { 
-            const decimal Pi = 3.14m;
-
-            decimal square = Pi * r * r;
-
-            return square;
-        }
     }
 
    
