2bc6cf0949409624973bf552cbc37b06e1f203a2
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-03-30 14:33:00 +0200

    reth

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -10,21 +10,18 @@ namespace Practis_Task1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Input your radius");
-            //string radiusString = Console.ReadLine();
-            //int.TryParse(radiusString, out int radius);
-            //Console.WriteLine($"The square of the round {Program.PerformCalcul(radius)}");
-            //Console.ReadKey();
             //task 1
             int uberful = 5;
             int _Identifier = 4;
             int \u006fldenifier;
             int myVariab1le;
+
             //task 2
             double pi = 3.141592653;
             double e = 2.7182818284590452;
             Console.WriteLine($"Pi = {pi}, E = {e}");
             //task 3
+
             string first = "\nмоя строка 1";
             string second = "\tмоя строка 2";
             string third = "\aмоя строка 3";
