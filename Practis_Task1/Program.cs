4b2d64e16528566453fc5816b33ee6507789d5f1
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-04 23:03:37 +0300

    Task1

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -12,7 +12,13 @@ namespace Practis_Task1
         {
             Console.WriteLine("Список задач : ");
             Console.WriteLine("Введите название задачи к решению которого вы хотите перейти (от 1 до 9) ");
-
+            int choose = Convert.ToInt32(Console.ReadLine());
+            if (choose == 1)
+            {
+                Console.WriteLine("Добро пожаловать в задачу 1");
+                int x1 = 255;
+                Console.WriteLine($"Целочисленная переменная x1 = {x1}");
+            }
             Console.ReadKey();
         }
 
