1fc45cd407c904904dd2f53d5187e20bad28b628
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-06 11:19:56 +0300

    Task2

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -10,7 +10,8 @@ namespace Practis_Task1
     {
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
+            Task2();
 
         }
         public static void Task1()
@@ -22,6 +23,30 @@ namespace Practis_Task1
             int second = Convert.ToInt32(Console.ReadLine());
             int hours = second / 3600;
             Console.WriteLine($"Number of hours = {hours}");
+        }
+        public static void Task2()
+        {
+            //
+            //Даны 3 числа: A,B,C; Проверить истинность высказывания Данное число находиться между числами А и С
+            //
+            Console.WriteLine("Input А value");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input B value");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input C value");
+            int c = Convert.ToInt32(Console.ReadLine());
+            if (b < a && b > c)
+            {
+                Console.WriteLine("B value locate between A and C");
+            }
+            else if (b > a && b < c)
+            {
+                Console.WriteLine("B value locate between A and C");
+            }
+            else
+            {
+                Console.WriteLine("B value doesn't locate between A and C");
+            }
         }
         
     }
