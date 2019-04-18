747514396a589dd4be27c3392cf5371f08f53596
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-18 22:11:42 +0300

    temp

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -10,7 +10,11 @@ namespace Practis_Task1
     {
         static void Main(string[] args)
         {
-            Determine_Symvol();
+            //Determine_Symvol();
+
+            //Change_Position();
+
+            Delete_Spaces_Between_Signs();
         }
 
         public static void Determine_Symvol()
@@ -32,12 +36,45 @@ namespace Practis_Task1
             Console.Write($"Number of duplicate symvol = {counter}");
         }
 
+        public static void Change_Position()
+        {
+            Console.WriteLine("Inpute some symvols to your string");
+            string first = Console.ReadLine();
+            Console.WriteLine("Inpute some symvols to your second string");
+            string second = Console.ReadLine();
+            Console.WriteLine("Inpute symvol position that you want split our strings");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Resalt = " + first.Insert(position, second));
+        }
+
+        public static void Delete_Spaces_Between_Signs()
+        {
+            string test = "How are you? Are you fine? Yes!";
+            string second = "";
+            char quation = '?';
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write(test[i]);
+                if (test[i] == quation)
+                {
+                    second = test[i];
+                }
+            }
+            //string[] second = test.Split(' ');
+            //foreach (var space in second)
+            //{
+            //    Console.Write($"{space}");
+            //}
+
+        }
     }
 }      
 
 
            
-
+//string[] textWithDeletedSpaces = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+//            Console.WriteLine(string.Join(" ", textWithDeletedSpaces));
 
 
    
