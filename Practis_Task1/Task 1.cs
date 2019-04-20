bf52d76d446deca59cc160204d37359e4ceddae4
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-20 13:11:35 +0300

    task1_final

--- Practis_Task1/Task 1.cs
+++ Practis_Task1/Task 1.cs
@@ -10,14 +10,46 @@ namespace Practis_Task1
     {
         public static void Calculate()
         {
-            Console.WriteLine("Input first value");
-            double first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input second value");
-            double second = Convert.ToInt32(Console.ReadLine());
+            double[] arrayOfArg = GetNumbers();
+            double first = arrayOfArg[0];
+            double second = arrayOfArg[1];
+            //Console.WriteLine("Input first value");
+            //double first = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input second value");
+            //double second = Convert.ToInt32(Console.ReadLine());
             char sign = ChooseOperation();
             //Console.WriteLine(sign);
             double result = ResaltOfOperation(first, second, sign);
             Console.WriteLine(result);
+        }
+
+        static double[] GetNumbers()
+        {
+            double[] array = new double[2];
+            while (true)
+            {
+                Console.WriteLine("Add first number: ");
+                if (Int32.TryParse(Console.ReadLine(), out int operand1))
+                {
+                    array[0] = operand1;
+                }
+                else
+                {
+                    Console.WriteLine("Add correct value");
+                    continue;
+                }
+                Console.WriteLine("Add second number: ");
+                if (Int32.TryParse(Console.ReadLine(), out int operand2))
+                {
+                    array[1] = operand2;
+                }
+                else
+                {
+                    Console.WriteLine("Add correct value");
+                    continue;
+                }
+                return array;
+            }
         }
 
         public static double AddOperation(double first, double second)
---
+++ Practis_Task1/Task_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    class Task_2
+    {
+        public static void ConvertOfCurency()
+        {
+        }
+    }
+}
