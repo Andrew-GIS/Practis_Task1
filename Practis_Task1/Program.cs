5811e855423ee838ebb12a4254429137676d15ff
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-20 12:10:47 +0300

    Task1_ Calculator

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -10,71 +10,17 @@ namespace Practis_Task1
     {
         static void Main(string[] args)
         {
-            //Determine_Symvol();
-
-            //Change_Position();
-
-            Delete_Spaces_Between_Signs();
+            Task_1.Calculate();
         }
 
-        public static void Determine_Symvol()
-        {
-            Console.WriteLine("Inpute some symvols ");
-            string test = Console.ReadLine();
-            Console.WriteLine("Symvols that you want to found ");
-            char found = Convert.ToChar(Console.ReadLine());
-            int counter = 0;
-            for (int i = 0; i < test.Length; i++)
-            {
-                //Console.Write(test[i]);
-                if (test[i] == found)
-                {
-                    counter++;
-                }
-                
-            }
-            Console.Write($"Number of duplicate symvol = {counter}");
-        }
+        
 
-        public static void Change_Position()
-        {
-            Console.WriteLine("Inpute some symvols to your string");
-            string first = Console.ReadLine();
-            Console.WriteLine("Inpute some symvols to your second string");
-            string second = Console.ReadLine();
-            Console.WriteLine("Inpute symvol position that you want split our strings");
-            int position = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Resalt = " + first.Insert(position, second));
-        }
-
-        public static void Delete_Spaces_Between_Signs()
-        {
-            string test = "How are you? Are you fine? Yes!";
-            string second = "";
-            char quation = '?';
-            for (int i = 0; i < test.Length; i++)
-            {
-                Console.Write(test[i]);
-                if (test[i] == quation)
-                {
-                    second = test[i];
-                }
-            }
-            //string[] second = test.Split(' ');
-            //foreach (var space in second)
-            //{
-            //    Console.Write($"{space}");
-            //}
-
-        }
     }
 }      
 
 
-           
-//string[] textWithDeletedSpaces = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-//            Console.WriteLine(string.Join(" ", textWithDeletedSpaces));
+
 
 
    
---
+++ Practis_Task1/Task 1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    public static class Task_1
+    {
+        public static void Calculate()
+        {
+            Console.WriteLine("Input first value");
+            double first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second value");
+            double second = Convert.ToInt32(Console.ReadLine());
+            char sign = ChooseOperation();
+            //Console.WriteLine(sign);
+            double result = ResaltOfOperation(first, second, sign);
+            Console.WriteLine(result);
+        }
+
+        public static double AddOperation(double first, double second)
+        {
+            double sum = 0;
+            sum = first + second;
+            return (sum);
+        }
+        public static double SubOperation(double first, double second)
+        {
+            double sub = 0;
+            sub = first - second;
+            return (sub);
+        }
+        public static double MulOperation(double first, double second)
+        {
+            double mul = 0;
+            mul = first * second;
+            return (mul);
+        }
+        public static double DivOperation(double first, double second)
+        {
+            double div = 0;
+            if (first == 0 || second == 0)
+            {
+                Console.WriteLine("Error - one of you values is 0");
+            }
+            else
+            {
+                div = first + second;
+            }
+            return (div);
+        }
+        public static char ChooseOperation()
+        {
+            Console.WriteLine("You must choose what you want to do with our value");
+            char sign = Convert.ToChar(Console.ReadLine());
+            return sign;
+        }
+        public static double ResaltOfOperation(double first, double second, char sign)
+        {
+            double result = 0;
+            switch (sign)
+            {
+                case '+':
+                    result = AddOperation(first, second);
+                    break;
+                case '-':
+                    result = SubOperation(first, second);
+                    break;
+                case '*':
+                    result = MulOperation(first, second);
+                    break;
+                case '/':
+                    result = DivOperation(first, second);
+                    break;
+                default:
+                    Console.WriteLine("Return");
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
