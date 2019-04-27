60a0f2d2449eb45c10d60ba90c62523898d6cf8a
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-04-27 12:13:40 +0300

    Task1

---
+++ Practis_Task1/Train.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    public struct Train
+    {
+        /// <summary>
+        /// /
+        /// </summary>
+        private int trainNumber;
+
+        private string destination;
+
+        private DateTime departureTime;
+
+        public Train(int trainNumber, string destination, DateTime departureTime)
+        {
+            if (trainNumber == 0)
+            {
+                throw new ArgumentException(nameof(trainNumber));
+            }
+            if (string.IsNullOrWhiteSpace(destination))
+                throw new ArgumentException(nameof(destination));
+            this.trainNumber = trainNumber;
+            this.destination = destination;
+            this.departureTime = departureTime;
+        }
+    }
+}
---
+++ Practis_Task1/TrainsScheduler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    public class TrainsScheduler
+    {
+        private Train GetNewTrain()
+        {
+            int numbers;
+            while (true)
+            {
+                Console.WriteLine("Eter the number of the train");
+                if (int.TryParse(Console.ReadLine(), out numbers))
+                {
+                    break;
+                }
+            }
+            DateTime departureTime;
+            while (true)
+            {
+                Console.WriteLine("Eter the departure time of the train");
+                if (DateTime.TryParse(Console.ReadLine(), out departureTime))
+                {
+                    break;
+                }
+            }
+           
+            string destination = string.Empty;
+            while (true)
+            {
+                Console.WriteLine("Eter the destination of the train");
+
+                destination = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(Console.ReadLine()))
+                {
+                    break;
+                }
+            }
+
+            return new Train(numbers, destination, departureTime);
+        }
+    }
+}
