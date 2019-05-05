27ad73c87361e5a6d6bf0369ad1c066a86487420
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-05-05 20:00:00 +0300

    temp

---
+++ Practis_Task1/Bicycle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    class Bicycle: Vehicle
+    {
+        public BicycleType Type { get; set; }
+        public BreakeType Breake { get; set; }
+
+        public Bicycle(BicycleType type, BreakeType breake,
+            int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount)
+            : base(wheelCount, color, maxSpeed, passengerCount)
+        {
+            Type = type;
+            Breake = breake;
+        }
+
+    }
+    enum BicycleType
+    {
+        RoadBike,
+        MountainBike,
+        BMX
+    }
+
+    enum BreakeType
+    {
+        HandBrakes,
+        FootBrakes
+    }
+}
---
+++ Practis_Task1/Vehicles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    class Vehicle
+    {
+        private int wheelCount = 1;
+        public ConsoleColor Color { get; set; }
+        private int maxSpeed = 1;
+        public int PassengerCount { get; set; }
+
+        public int WheelCount
+        {
+            get { return wheelCount; }
+            set
+            {
+                if (value > 0)
+                {
+                    wheelCount = value;
+                }
+            }
+        }
+
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Speed value should be more than 0");
+                }
+                else maxSpeed = value;
+            }
+        }
+
+        public Vehicle(int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount)
+        {
+            WheelCount = wheelCount;
+            Color = color;
+            MaxSpeed = maxSpeed;
+            PassengerCount = passengerCount;
+        }
+
+        public void Move(int metres)
+        {
+            Console.ForegroundColor = Color; // color of console
+            Console.WriteLine($"{GetType().Name} has moved {metres} meters");
+        }
+    }
+}
