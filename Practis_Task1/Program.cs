87f748fc00112970c78c886bb45f2bfd6780e9f0
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-05-31 22:37:17 +0300

    task3 - vowels letter count

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Practis_Task1.Task1;
+using Practis_Task1.Task3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,13 @@ namespace Practis_Task1
             //
             //Task1 - upper case task.
             //
-            Jaden_Smith text = new Jaden_Smith();
-            text.WriteLikeJSmith();
+            //Jaden_Smith text = new Jaden_Smith();
+            //text.WriteLikeJSmith();
+            //
+            //Task3 - Founding of vowels letters
+            //
+            Vowels vowels = new Vowels();
+            vowels.FoundingVowels();
         }
     }
 }
