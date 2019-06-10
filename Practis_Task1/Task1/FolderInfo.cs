0b038a2808e3c22de644dbb10540f32313f2ec2a
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-06-10 22:21:37 +0300

    Task2

---
+++ Practis_Task1/Task1/FolderInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1.Task1
+{
+    class FolderInfo
+    {
+        //
+        // вызвать рекусрсивный метод по нахождению обьема
+        //
+        public void DisplayInfo()
+        {
+            var directory = "D:\\test";
+            int counter = 0;
+            long size = 0;
+
+            Console.WriteLine("Name of your folders: ");
+            Console.WriteLine(string.Empty);
+            foreach (var item in Directory.GetDirectories(directory))
+            {
+                //Console.WriteLine(item);
+                counter++;
+            }
+            Console.WriteLine(string.Empty);
+            Console.WriteLine($"You folder contains {counter} folders");
+
+            int counter2 = 0;
+            DirectoryInfo info = new DirectoryInfo(directory);
+            foreach (var item in Directory.GetFiles(directory))
+            {
+                Console.WriteLine(item);
+                size += item.Length;
+                counter2++;
+            }
+            Console.WriteLine($"Files - {counter2}");
+            Console.WriteLine($"Size of your directory - {size} bite(s)");
+        }
+    }
+}
