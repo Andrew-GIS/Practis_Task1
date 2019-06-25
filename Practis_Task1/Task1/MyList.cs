27135b2b2d92dc35faa79a34973a82b9712b3a33
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-06-25 20:12:37 +0300

    temp

---
+++ Practis_Task1/Task1/MyList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1.Task1
+{
+    class MyList<T> : IMyList<T>
+    {
+        public T[] array { get; set; }
+
+        public MyList()
+        {
+            T[] array = new T[0];
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = array.Length;
+                return count;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.array[index];
+            }
+        }
+
+        public void Add(T a)
+        {
+            T[] arr = new T[array.Length + 1];
+            array.CopyTo(arr, 0);
+            array = arr;
+            array[array.Length - 1] = a;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
+public interface IMyList<T>
+
+{
+    void Add(T a);
+
+    T this[int index] { get; }
+
+    int Count { get; }
+
+    void Clear();
+
+    bool Contains(T item);
+}
