ec65100cf282fa5eda416046c88433684534bd9b
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-06-28 13:51:43 +0300

    Task1

---
+++ Practis_Task1/Task1_/Student.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1.Task1_
+{
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string cours;
+        private string groupe;
+        private string faculty;
+
+        public string FirstName
+        { get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Cours
+        {
+            get
+            {
+                return cours;
+            }
+            set
+            {
+                cours = value;
+            }
+        }
+        
+        public string Groupe
+        {
+            get
+            {
+                return groupe;
+            }
+            set
+            {
+                groupe = value;
+            }
+        }
+        
+        public string Faculty
+        {
+            get
+            {
+                return faculty;
+            }
+            set
+            {
+                faculty = value;
+            }
+        }
+
+        public Student(string firstname, string lastName, string cours, string groupe, string faculty)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastName;
+            this.Cours = cours;
+            this.Groupe = groupe;
+            this.Faculty = faculty;
+        }
+
+        public override string ToString()
+        {
+            return this.FirstName + ", " + this.LastName + ", " + Cours + ", " + Groupe + ", " + Faculty + ".";
+        }
+
+    }
+}
---
+++ Practis_Task1/Task1_/StudentCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1.Task1_
+{
+    class StudentCollection 
+    {
+        List<Student> students = new List<Student> {
+        new Student("Ivan", "Petrov", "3", "432", "4"),
+        new Student("Maria", "Sklar", "2", "324", "3"),
+        new Student("Petro", "Johnson", "1", "512", "5"),
+        new Student("Andrey", "Vosk", "6", "662", "6")};
+
+        public void AddStudent ()
+        {
+            students.Add(new Student("Ivan", "Petrov", "3", "432", "4"));
+        }
+
+        public void RemoveStudent()
+        {
+            students.Remove(new Student("Ivan", "Petrov", "3", "432", "4"));
+        }
+
+        public void CountOfStudent()
+        {
+            int count = students.Count();
+            Console.WriteLine($"Number of students - {count}"); 
+        }
+
+        public void ShowStudents()
+        {
+            Console.WriteLine("List of students:");
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i].ToString());
+            }
+        }
+    }
+}
