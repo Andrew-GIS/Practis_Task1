32670b478cc7c9583a1235bbd150ae19aa482d2e
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-07-14 00:24:26 +0300

    MammalODD - finish

---
+++ Practis_Task1/Terrestrial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practis_Task1
+{
+    class Terrestrial : Mammals
+    {
+        public Terrestrial(double age, double weight) :
+            base(age, weight)
+        {
+
+        }
+    }
+}
