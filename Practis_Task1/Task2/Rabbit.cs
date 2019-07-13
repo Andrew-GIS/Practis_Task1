e79ddce1dc6b707701ce26c2af235cee00fb6445
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-07-13 16:17:07 +0300

    temp

--- Practis_Task1/Task2/Rabbit.cs
+++ Practis_Task1/Task2/Rabbit.cs
@@ -8,7 +8,19 @@ namespace Practis_Task1.Task2
 {
     class Rabbit
     {
-        public int longitude { get; set; }
-        public int latitude { get; set; }
+        private int locationX { get; set; }
+        private int locationY { get; set; }
+
+        public Rabbit(int longitude, int latitude)
+        {
+            this.locationX = longitude;
+            this.locationY = latitude;
+        }
+
+        public void ChengeLocation(int locationX, int locationY)
+        {
+            Random rnd = new Random();
+            locationX = rnd.
+        }
     }
 }
