28d9b41140e621386c494245004bd05fe7d61d04
Андрей Федорченко <fedorchenko.andrey@hotmail.com>
2019-08-14 00:25:06 +0300

    Task3

--- Practis_Task1/Program.cs
+++ Practis_Task1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Practis_Task1.Task1;
 using Practis_Task1.Task1._2;
 using Practis_Task1.Task2;
+using Practis_Task1.Task3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,11 +24,18 @@ namespace Practis_Task1
             //
             //Hunting.
             //
-            HuntingProcess hunting = new HuntingProcess();
-            hunting.DoHuntingProcess();
+            //HuntingProcess hunting = new HuntingProcess();
+            //hunting.DoHuntingProcess();
 
+            //
+            //IDisposable realization
+            //
+            TestSpace test1 = new TestSpace();
+            test1.Check();
+            TestSpace test2 = new TestSpace();
+            test2.Check2();
         }
-        
+
     }
 }      
 
---
+++ Practis_Task1/Task3/Pupil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practis_Task1.Task3
+{
+    class Pupil : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("Disposing");
+            for (int i = 1; i <= 3; i++)
+            {
+                Thread.Sleep(500);
+                Console.Beep();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            Dispose();
+        }
+    }
+}
